c08316b7cffd4c38b2b5b6c6b5d40835a19ae38e
CamiloMaria <camilocraft08@gmail.com>
2024-04-12 00:14:29 -0400

    Add new pages and routes for Explore, AllUsers, CreatePost, EditPost, PostDetails, Profile, Saved, and UpdateProfile

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import "./globals.css";
 import { Routes, Route } from "react-router-dom";
 import { SigninForm, SignupForm } from "./_auth/forms";
 import { Toaster } from "./components/ui/toaster";
-import { Home } from "./_root/pages";
+import {
+	AllUsers,
+	CreatePost,
+	EditPost,
+	Explore,
+	Home,
+	PostDetails,
+	Profile,
+	Saved,
+	UpdateProfile,
+} from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 
@@ -19,6 +29,14 @@ const App = () => {
 				{/* private routes */}
 				<Route element={<RootLayout />}>
 					<Route index element={<Home />} />
+					<Route path="/explore" element={<Explore />} />
+					<Route path="/saved" element={<Saved />} />
+					<Route path="/all-users" element={<AllUsers />} />
+					<Route path="/create-post" element={<CreatePost />} />
+					<Route path="/update-post/:id" element={<EditPost />} />
+					<Route path="/posts/:id" element={<PostDetails />} />
+					<Route path="/profile/:id/*" element={<Profile />} />
+					<Route path="/update-profile/:id" element={<UpdateProfile />} />
 				</Route>
 			</Routes>
 
