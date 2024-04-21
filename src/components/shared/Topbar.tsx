c5032946a5cb665c51fe2596082dee8872336353
MARÍA CASTILLO, CAMILO JOSÉ <63072095+CamiloMaria@users.noreply.github.com>
2024-04-20 23:17:28 -0400

    Update Topbar.tsx

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -19,9 +19,6 @@ const Topbar = () => {
 	return (
 		<section className="topbar">
 			<div className="flex-between py-4 px-5">
-				<Link to="/" className="flex gap-3 items-center">
-					<img src="/assets/images/logo.svg" alt="logo" width={130} height={325} />
-				</Link>
 
 				<div className="flex gap-4">
 					<Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
